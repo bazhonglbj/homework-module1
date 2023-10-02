7b7e18786faf381c04596fad5cefb0e20bc17617
bazhonglbj <bazhonglbj@gmail.com>
2023-10-01 17:27:34 -0700

    add index content

--- js/activity.js
+++ js/activity.js
@@ -3,14 +3,18 @@ $(document).ready(function(){
 
     $(".selectable").click(function() {
         var content = $(this).text();
+        var colIndex = $(this).index();
+        var header = $("table thead th").eq(colIndex).text();
+        var displayText = content + " at " + header;
+
         $(this).toggleClass("highlighted");
 
         if ($(this).hasClass("highlighted")){
             $('#displaySelected').css("visibility","visible");
             $('#displaySelected').css("margin-top","2-em");
-            $('#result').append("<p>"+ content + "</p>");
+            $('#result').append("<p>"+ displayText + "</p>");
         } else {
-            $('#result p:contains('+ content +')').remove();
+            $('#result p:contains('+ displayText +')').remove();
 
             if($('#result').has('p').length==false){
                 $('#displaySelected').css("visibility","hidden");
