8b16353b18c395de8aaf192a87d600990e3e4a5e
bazhonglbj <bazhonglbj@gmail.com>
2023-10-01 17:22:34 -0700

    initiate selected

--- js/activity.js
+++ js/activity.js
@@ -2,6 +2,20 @@ $(document).ready(function(){
     $("td").not(":contains('Not Available')").addClass("selectable");
 
     $(".selectable").click(function() {
+        var content = $(this).text();
         $(this).toggleClass("highlighted");
+
+        if ($(this).hasClass("highlighted")){
+            $('#displaySelected').css("visibility","visible");
+            $('#displaySelected').css("margin-top","2-em");
+            $('#result').append("<p>"+ content + "</p>");
+        } else {
+            $('#result p:contains('+ content +')').remove();
+
+            if($('#result').has('p').length==false){
+                $('#displaySelected').css("visibility","hidden");
+                $('#displaySelected').css("margin-top","0");
+            }
+        }
     });
 });
